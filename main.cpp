3a2435334c05d2b697f6a851be635ff963baa593
ontheroad530 <ontheroad530@github.com>
2016-04-19 10:55:37 +0800

    update queue

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
-#include "main.h"
+#include "myqueue.h"
 
-main::main()
+#include <unistd.h>
+
+MyQueue<int>    myQueue(1024);
+MyQueue<int>    myBak(0);
+void test_bak();
+void test_queue();
+
+int main()
 {
+    std::thread t(test_queue);
+    std::thread t2(test_bak);
 
+    t.join();
+    t2.join();
+
+    return 0;
 }
 
+void test_queue()
+{
+    usleep(1000);
+    while ( !myQueue.empty() )
+    {
+        int* a = myQueue.malloc();
+        myBak.free(a);
+        std::cout << "[S: " << myQueue.size() << "] ";
+        usleep(1000);
+    }
+}
+
+void test_bak()
+{
+    usleep(1000*100);
+    while ( !myBak.empty() ) {
+        int*a = myBak.malloc();
+        myQueue.free(a);
+        std::cout << "[B: " << myBak.size() << "] ";
+        usleep(1000);
+    }
+}
--- myqueue.h
+++ myqueue.h
@@ -1,11 +1,59 @@
 #ifndef MYQUEUE_H
 #define MYQUEUE_H
 
+#include <list>
+#include <mutex>
+#include <thread>
+#include <iostream>
+#include <condition_variable>
 
+
+template<typename T>
 class MyQueue
 {
 public:
-    MyQueue();
+    MyQueue(size_t queue_size) :m_queueSize(queue_size)
+    {
+        for(int i = 0; i < queue_size; ++i)
+        {
+            m_queue.push_back( new T() );
+        }
+    }
+
+    T*  malloc()
+    {
+        std::unique_lock<std::mutex> locker(m_mutex);
+        m_notEmpty.wait( locker, [this]{ return !m_queue.empty(); } );
+        T* x = m_queue.front();
+        m_queue.pop_front();
+
+        return x;
+    }
+
+    void free(T* t)
+    {
+        std::lock_guard<std::mutex> locker(m_mutex);
+        m_queue.push_back(t);
+        m_notEmpty.notify_one();
+    }
+
+    bool empty()
+    {
+        std::lock_guard<std::mutex> locker(m_mutex);
+        return m_queue.empty();
+    }
+
+    size_t size()
+    {
+        std::lock_guard<std::mutex> locker(m_mutex);
+        return m_queue.size();
+    }
+
+private:
+    size_t     m_queueSize;
+    std::mutex  m_mutex;
+    std::list<T*>    m_queue;
+    std::condition_variable m_notEmpty;
 };
 
 #endif // MYQUEUE_H
